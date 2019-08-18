e284874192260853db8a625a786f97cf0b146361
erez <gilronerez@gmail.com>
2019-08-18 06:06:16 +0300

    fixed crontab issues

--- cronjob.py
+++ cronjob.py
@@ -9,18 +9,21 @@ def get_all_user_data():
     print("hi")
     for user in all_users:
         user_id = str(user[0])
+        if user_id == '':
+            continue
         user_channels = mysql_connector.get_user_channels(user_id=user_id)
         print(user_channels)
         for user_channel in user_channels:
             channel = user_channel[0]
             channel_type = user_channel[1]
+            results = []
             last_aggregation = mysql_connector.get_last_aggregation_for_user_channel(user_id, channel)
             if channel_type == "api":
-                api_scraper.fetch_financial_data(pre_last_date=last_aggregation, user_id=user_id, channel=channel)
+                results.append(api_scraper.fetch_financial_data(pre_last_date=last_aggregation, user_id=user_id, channel=channel))
             elif channel_type == "website":
-                selenium_scraper.fetch_financial_data(pre_last_date=last_aggregation, user_id=user_id, channel=channel)
-    # after getting all channels data, get all statements
-    statement_handler.get_statments(user_id)
+                results.append(selenium_scraper.fetch_financial_data(pre_last_date=last_aggregation, user_id=user_id, channel=channel))
+        # after getting all channels data, get all statements
+        statement_handler.get_statments(user_id)
 
 
 get_all_user_data()
